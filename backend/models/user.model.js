3bc3e4394530d6ca6c39ee70c57810901c0a99ea
lelisa21 <lelisahailu21@gmail.com>
2026-03-11 20:37:05 +0300

    feat: some intialization codes to start

---
+++ backend/models/user.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose"
+
+const userSchema = new mongoose.Schema({
+    username:{
+       type:String,
+       required:[true, "Username is required"],
+       minLength:3 
+    },
+    email:{
+       type:String,
+       required:[true, "email is required"],
+       unique:true
+
+    },
+    password:{
+       type:String,
+       required:[true, "password field is required"],
+       minLength:6,
+    }
+
+})
+
+
+const User = mongoose.model("User" , userSchema)
+
+export default User
